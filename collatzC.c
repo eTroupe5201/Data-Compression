01d91467acb9025961c6822e90b5759ba3244173
eTroupe5201 <100246757+eTroupe5201@users.noreply.github.com>
2022-03-02 13:46:53 -0600

    Create collatzC.c

---
+++ collatzC.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int sequence(long n){
+int count = 1;
+  while(n != 1){
+  if(n % 2 == 0){
+  n /= 2;
+  }else{
+  n = (3 * n) + 1;
+  }
+    ++count;
+  }
+  return count;
+}
+  int main(int argc, char *argv[]){
+  int max = 0; maxi;
+    for(int i = 999999, i > 0; --i){
+    int s = sequence(i);
+      if(s > max){
+       max = s;
+       maxi = i;
+      }
+    }
+    printf("%d", maxi);
+  }
