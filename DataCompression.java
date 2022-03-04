ff8d5d51573513251c5d48bf8cef66f0d283069f
eTroupe5201 <100246757+eTroupe5201@users.noreply.github.com>
2022-03-03 20:11:16 -0600

    Create DataCompression.java

---
+++ DataCompression.java
@@ -0,0 +1,14 @@
+package dataCompression;
+
+import java.io.IOException;
+
+public class DataCompression {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+	
+		new GuiUserInterface();
+	
+	}
+
+}
